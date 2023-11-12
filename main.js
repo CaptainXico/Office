75d0713410065165814f75ccabaf706bf66d7270
CaptainXico <f.cardoso.f94@gmail.com>
2023-11-12 21:58:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js
 
 // Set up Three.js scene. import GLTFLoader
-//import { GLTFLoader } from 'three';
+import { GLTFLoader } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
